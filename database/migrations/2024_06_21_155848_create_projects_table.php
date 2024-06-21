ee20dac92f7e8efcab239634ca9798c2fdb65654
arrovain <arrovain@gmail.com>
2024-06-21 19:03:12 +0300

    generate colums for project

--- database/migrations/2024_06_21_155848_create_projects_table.php
+++ database/migrations/2024_06_21_155848_create_projects_table.php
@@ -22,6 +22,16 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects');
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->timestamp('due_date')->nullable();
+            $table->string('status');
+            $table->string('image_path')->nullable();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->timestamps();
+        });
     }
 };
