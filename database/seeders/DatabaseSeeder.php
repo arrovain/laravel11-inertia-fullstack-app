6e4697a7396491793f0b0378eeb4f939a528dff8
arrovain <arrovain@gmail.com>
2024-06-21 19:11:13 +0300

    create user factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'icme',
+            'email' => 'icme@example.com',
+            "password" => bcrypt("icmeicme"),
+            
         ]);
     }
 }
