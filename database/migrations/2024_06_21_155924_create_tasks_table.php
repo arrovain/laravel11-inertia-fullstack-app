f9e826b2b3609572ca0a20d4e081641d27b07f3a
arrovain <arrovain@gmail.com>
2024-06-21 19:04:09 +0300

    generate colums for tasks

--- database/migrations/2024_06_21_155924_create_tasks_table.php
+++ database/migrations/2024_06_21_155924_create_tasks_table.php
@@ -22,6 +22,19 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tasks');
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->string('image_path')->nullable();
+            $table->string('status');
+            $table->string('priority');
+            $table->string('due_date')->nullable();
+            $table->foreignId('assigned_user_id')->constrained('users');
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->foreignId('project_id')->constrained('projects');
+            $table->timestamps();
+        });
     }
 };
