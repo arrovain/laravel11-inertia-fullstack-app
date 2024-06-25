930722b3f0d22e2ac3366c4b02b3f2ed19cee68f
arrovain <arrovain@gmail.com>
2024-06-25 16:43:15 +0300

    accept auth and projects

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -1,12 +1,14 @@
 import Authenticated from "@/Layouts/AuthenticatedLayout";
 
-export default function Index() {
+export default function Index({ auth, projects }) {
   return (
     <Authenticated
       user={auth.user}
       header={
         <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"></h2>
       }
-    ></Authenticated>
+    >
+      Projects
+    </Authenticated>
   );
 }
