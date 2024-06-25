17a8bb9187d96bae3afc3c77789e502d8f27bfb4
arrovain <arrovain@gmail.com>
2024-06-25 16:19:41 +0300

    Authenticated layout and 2 props

---
+++ resources/js/Pages/Project/index.jsx
@@ -0,0 +1,12 @@
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+
+export default function Index() {
+  return (
+    <Authenticated
+      user={auth.user}
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"></h2>
+      }
+    ></Authenticated>
+  );
+}
