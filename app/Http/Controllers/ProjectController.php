9e2de1b1b9ec034dad6a852b7598511e75dee320
arrovain <arrovain@gmail.com>
2024-06-25 16:52:12 +0300

    paginate

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -14,7 +14,7 @@ class ProjectController extends Controller
     public function index()
     {
         $query = Project::query();
-        
+        $projects = $query->paginate(10)->onEachPage(1);
         return inertia("Project/Index", []);
     }
 
